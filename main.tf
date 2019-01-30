4987a6c4d2dfac85c4045f2a34c5dd17e2022b05
Ryan McCaul <Ryan.McCaul@tmx.com>
2019-01-30 14:36:30 -0500

    no message

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "consul_lb_aws" {
   cidr_blocks        = ["${var.public ? "0.0.0.0/0" : var.vpc_cidr}"] # If there's a public IP, open port 22 for public access - DO NOT DO THIS IN PROD
   subnet_ids         = ["${var.subnet_ids}"]
   is_internal_lb     = "${!var.public}"
-  use_lb_cert        = "${var.use_lb_cert}"
+  use_lb_cert        = "${var.use_lb_cert}" 
   lb_cert            = "${var.lb_cert}"
   lb_private_key     = "${var.lb_private_key}"
   lb_cert_chain      = "${var.lb_cert_chain}"
